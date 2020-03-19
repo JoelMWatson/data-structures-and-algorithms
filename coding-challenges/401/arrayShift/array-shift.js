8fc101cfd6304fdbe84d519130d087f0f119b6be
Joel Watson <Joel.watson92@gmail.com>
2020-03-19 14:52:56 -0700

    Finished array-shift challenge

--- coding-challenges/401/arrayShift/array-shift.js
+++ coding-challenges/401/arrayShift/array-shift.js
@@ -1,2 +1,18 @@
 'use strict'
 
+const insertShiftArray = (arr, num) => {
+    let newArr = [];
+    let half = Math.ceil(arr.length/2);
+    for (let i=0; i < arr.length+1; i++) {
+        if (i < half) {
+            newArr[i] = arr[i];
+        } else if (i > half) {
+            newArr[i] = arr[i-1]
+        } else {
+            newArr[i] = num; 
+        }
+    }
+    return newArr;
+}
+
+module.exports = insertShiftArray;
