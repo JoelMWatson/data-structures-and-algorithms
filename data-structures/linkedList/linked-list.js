35d099e435c8bf2cad28dd9f07b55537257abd15
Joel Watson <Joel.watson92@gmail.com>
2020-03-29 21:35:59 -0700

    Finished lab 05

--- data-structures/linkedList/linked-list.js
+++ data-structures/linkedList/linked-list.js
@@ -13,16 +13,54 @@ class LinkedList {
         this.head = null;
     }
 
+    count() {
+        if (!this.head) {
+            return 0;
+        }
+        let current = this.head;
+        let count = 1;
+        while(current.next) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
     insert(val) {
-        
+        if (!this.head) {
+            this.head = new Node(val);
+        } else {
+            let newHead = new Node(val, this.head);
+            this.head = newHead;
+        }
     }
 
     includes(val) {
-        
+        let current = this.head;
+        let verdict = false;
+        while (current) {
+            if (current.val === val) {
+                verdict = true;
+                break;
+            }
+            current = current.next;
+        }
+        return verdict;
     }
 
     toString() {
-
+        if (!this.head) {
+            return '';
+        }
+        let current = this.head;
+        let str = ''
+        while (current) {
+            if (str !== '') str += ' -> ';
+            str += `{ ${current.val} }`;
+            current = current.next;
+        }
+        str += ' -> NULL'
+        return str;
     }
 }
 
@@ -32,5 +70,18 @@ class DoublyLinkedList extends LinkedList {
         this.tail = null;
     }
 }
+
+
+
+
+let list = new LinkedList();
+list.insert(10);
+list.insert(8);
+list.insert(6);
+console.log(list.toString());
+
+
+
+
 
 module.exports = { Node, LinkedList, DoublyLinkedList }
