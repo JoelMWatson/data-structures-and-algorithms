fb118c9cbbe745851ceed3de30711da5986179af
Joel Watson <Joel.watson92@gmail.com>
2020-04-08 14:49:32 -0700

    finished code challenge 08 stretch goal

--- coding-challenges/401/llMerge/ll-merge.js
+++ coding-challenges/401/llMerge/ll-merge.js
@@ -23,8 +23,8 @@ const mergeLists = (list1, list2) => {
     cur2.next = ref1;
 
     // iterate forward
-    cur1 = ref1;
     cur = cur2;
+    cur1 = ref1;
     cur2 = ref2;
   }
   if (cur2) {
@@ -34,4 +34,47 @@ const mergeLists = (list1, list2) => {
   return zipped;
 };
 
-module.exports = mergeLists;
+const sortedMergeLists = (list1, list2) => {
+  // create current vars for both lists
+  let cur1 = list1.head;
+  let cur2 = list2.head;
+  let merged;
+  if (cur1.val > cur2.val) {
+    merged = cur2;
+    cur2 = cur2.next;
+  } else {
+    merged = cur1;
+    cur1 = cur1.next;
+  }
+  let cur = merged;
+
+  while (cur1 && cur2) {
+    // create 2 reference vars to the current next node for each list
+    let ref1 = cur1.next;
+    let ref2 = cur2.next;
+    if (cur1.val > cur2.val) {
+      // set next in merged to cur2.
+      cur.next = cur2;
+
+      //iterate forward
+      cur = cur.next;
+      cur2 = ref2;
+    } else {
+      // set next in merged to cur1.
+      cur.next = cur1;
+
+      //iterate forward
+      cur = cur.next;
+      cur1 = ref1;
+    }
+  }
+  if (cur2) {
+    cur.next = cur2;
+  } else if (cur1) {
+    cur.next = cur1;
+  }
+  // return reference to the head of the merged list
+  return merged;
+};
+
+module.exports = { mergeLists, sortedMergeLists };
